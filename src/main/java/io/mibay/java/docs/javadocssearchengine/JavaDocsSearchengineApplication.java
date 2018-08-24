ad9c6e087afd99039eca807d9a013dfcfc1cb633
Mickaël <mickael.baye@gmail.com>
2018-08-24 16:52:52 +0200

    feat: initialize spring boot

---
+++ src/main/java/io/mibay/java/docs/javadocssearchengine/JavaDocsSearchengineApplication.java
@@ -0,0 +1,12 @@
+package io.mibay.java.docs.javadocssearchengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDocsSearchengineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDocsSearchengineApplication.class, args);
+	}
+}
---
+++ src/test/java/io/mibay/java/docs/javadocssearchengine/JavaDocsSearchengineApplicationTests.java
@@ -0,0 +1,16 @@
+package io.mibay.java.docs.javadocssearchengine;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavaDocsSearchengineApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
